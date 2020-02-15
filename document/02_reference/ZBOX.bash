944cb7cfcf76e862a4a92c50dee47813bebbe981
babyvox0621 <babyvox0621@gmail.com>
2020-02-15 17:04:51 +0900

    bash file created

--- document/02_reference/ZBOX.bash
+++ document/02_reference/ZBOX.bash
@@ -1,16 +1,6 @@
 #!/bin/bash
-rosrun TOP TOP &
-
-#!/bin/bash
-rosrun dummy_voice dummy_voice &
-
-#!/bin/bash
-rosrun merge_coordinate merge image_transport:=compressed
-
-#!/bin/bash
-roslaunch darknet_ros yolo_v3.launch &
-
-
-
-
-
+gnome-terminal --geometry=80x20+0+0 --window -e 'bash -c "rosrun TOP TOP;exec bash"'
+gnome-terminal --geometry=80x20+500+0 --window -e 'bash -c "rosrun merge_coordinate merge image_transport:=compressed;exec bash"'
+gnome-terminal --geometry=80x20+1000+0 --window -e 'bash -c "rosrun dummy_voice dummy_voice;exec bash"'
+gnome-terminal --geometry=80x20+0+400 --window -e 'bash -c "roslaunch darknet_ros yolo_v3.launch;exec bash"'
+gnome-terminal --geometry=80x20+500+400 --window -e 'bash -c "rosrun voice monitoring.py;exec bash"'
